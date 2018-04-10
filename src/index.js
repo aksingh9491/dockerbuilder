c82931a1a7b1be88a37d4034b3944299d462932e
Jami Kousa <jakousa@cs.helsinki.fi>
2018-04-10 14:42:11 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const PORT = process.env.PORT || 8083
+const app = express()
+
+app.get('/ping', (req, res) => {
+    res.status(200).send('pong').end()
+})
+
+app.post('/build', (req, res) => {
+    console.log(req.body)
+    res.status(200).end()
+})
+
+app.listen(PORT, () => console.log(`Server listening port ${PORT}`))
