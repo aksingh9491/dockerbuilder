5d85ce28de409fe9beb9276aae5bf6675d53cecd
Jami Kousa <jakousa@cs.helsinki.fi>
2018-04-12 15:20:17 +0300

    answer to any get

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ const PORT = process.env.PORT || 8080
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/ping', (req, res) => {
-    res.status(200).send('pong').end()
-})
-
 app.post('/build', (req, res) => {
     console.log(req.body)
     res.status(200).end()
+})
+
+app.get('*', (req, res) => {
+    console.log(req)
+    res.status(200).send('pong').end()
 })
 
 app.listen(PORT, () => console.log(`Server listening port ${PORT}`))
