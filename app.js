feb1166a64e9311f92f47d13f8cbcce002706141
Jami Kousa <jakousa@cs.helsinki.fi>
2018-04-12 18:46:07 +0300

    git clone & docker build

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import git from 'simple-git'
+import { spawn } from 'child_process'
+import path from 'path'
+
+const PORT = process.env.PORT || 8080
+const simpleGit = git()
+const app = express()
+app.use(bodyParser.json())
+
+
+const buildaa = async (req, res) => {
+    const push = req.body
+    const { clone_url } = push.repository
+    const { name } = push.repository
+    const here = path.resolve(__dirname)
+    const repositoryLocation = `${here}/repository/${name}`
+
+    const simpleGit = await git().clone(clone_url, repositoryLocation)
+    const process = spawn('docker', ['build', '.', '-t', name], { cwd: repositoryLocation })
+    process.stdout.on('data', (data) => {
+        console.log(`Process ${name}: ` + data);
+    });
+    process.on("close", (code, signal) => {
+        spawn('rm', ['-r', repositoryLocation])
+        // spawn('docker', ['push', ])
+    })
+
+    res.status(200).end()
+}
+
+app.post('/build', buildaa)
+
+// app.get('*', buildaa)
+
+app.listen(PORT, () => console.log(`Server listening port ${PORT}`))
