600cfeedb800385962bc4ba97ee4c44aea7d9fcd
Jami Kousa <jakousa@cs.helsinki.fi>
2018-04-13 15:07:46 +0300

    only build on master, support tags

--- app.js
+++ app.js
@@ -9,28 +9,55 @@ const simpleGit = git()
 const app = express()
 app.use(bodyParser.json())
 
+const buildImage = async (name, clone_url) => {
+    try {
+        const containerName = `localhost:5000/${name}:staging`
+        const here = path.resolve(__dirname)
+        const repositoryLocation = `${here}/repository/${name}`
 
-const buildaa = async (req, res) => {
+        const simpleGit = await git().clone(clone_url, repositoryLocation)
+        const process = spawn('docker', ['build', '.', '-t', containerName], { cwd: repositoryLocation })
+        process.stdout.on('data', (data) => {
+            if (data) {
+                console.log(`Process ${name}: ` + data);
+            }
+        });
+        process.on("close", (code, signal) => {
+            spawn('rm', ['-r', repositoryLocation])
+            spawn('docker', ['push', containerName])
+        })
+    } catch (e) {
+        console.log('Building image failed:', e)
+    }
+}
+
+const tagRelease = async (name) => {
+    try {
+        const containerName = `localhost:5000/${name}`
+        const newTag = spawn('docker', ['tag', `${containerName}:staging`, containerName])
+        newTag.on("close", (code, signal) => {
+            spawn('docker', ['push', containerName])
+        })
+    } catch (e) {
+        console.log('Releasing tag failed:', e)
+    }
+}
+
+const handlePush = async (req, res) => {
     const push = req.body
-    const { clone_url } = push.repository
-    const { name } = push.repository
-    const here = path.resolve(__dirname)
-    const repositoryLocation = `${here}/repository/${name}`
-
-    const simpleGit = await git().clone(clone_url, repositoryLocation)
-    const process = spawn('docker', ['build', '.', '-t', `localhost:5000/${name}`], { cwd: repositoryLocation })
-    process.stdout.on('data', (data) => {
-        console.log(`Process ${name}: ` + data);
-    });
-    process.on("close", (code, signal) => {
-        spawn('rm', ['-r', repositoryLocation])
-        spawn('docker', ['push', `localhost:5000/${name}`])
-    })
-
+    const { ref } = push
+    const { clone_url, name } = push.repository
+    if (!ref.includes('master')) {
+        if (!ref.includes('tags')) { // This is for our current setup, tags are only made on master branch.
+            tagRelease(name)
+        }
+        return res.status(200).end()
+    }
+    buildImage(name, clone_url)
     res.status(200).end()
 }
 
-app.post('/build', buildaa)
+app.post('/build', handlePush)
 
 // app.get('*', buildaa)
 
